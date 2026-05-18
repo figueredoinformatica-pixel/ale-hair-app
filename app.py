c193684f75520562d902e1f01145bca7fd63265e
figueredoinformatica-pixel <figueredoinformatica@gmail.com>
2026-05-18 18:11:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -319,7 +319,7 @@ df_servicos = pd.DataFrame(dados)
 
 if st.session_state.tela == "catalogo":
 
-    hero_path = "assets/corte.png"
+    hero_path = "assets/hero.jpg"
 
     if os.path.exists(hero_path):
 
